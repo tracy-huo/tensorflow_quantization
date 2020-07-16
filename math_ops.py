4796adeb2ef8b22bde7a65301663f4b22139f5ab
tracy-huo <58504357+tracy-huo@users.noreply.github.com>
2020-07-16 21:30:22 +0800

    Create math_ops.py

---
+++ math_ops.py
@@ -0,0 +1,17 @@
+"""Quantized Math Operations."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+from tensorflow.python.framework import common_shapes
+from tensorflow.python.framework import os
+from tensorflow.python.ops import gen_math_ops
+from tensorflow.python.ops.gen_math_ops import *
+
+
+
+
+
+
